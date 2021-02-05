4b7829272e0768148c9a466d3fb2e9111cb4f750
BigAgnes <k.tc.7@yandex.ru>
2021-02-05 22:23:21 +0300

    Change hard

--- HomeworkHard.java
+++ HomeworkHard.java
@@ -1,15 +1,21 @@
-import java.util.Arrays;
 import java.util.Random;
 
 public class HomeworkHard {
     public static void main(String[] args) {
         Random random = new Random();
-        int[] array = new int[50];
-
-        for (int i = 0; i < array.length; i++) {
-            array[i] = random.nextInt(10800);
-            Arrays.sort(array);
+        int[] array = new int[100];
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < 100; i++) {
+            array[i]=random.nextInt(1000);
         }
-        System.out.println(array[0]);
+        for (int i = 0; i < 100; i++) {
+            if (array[i] < min) min = array[i];
+        }
+        System.out.println(min);
+        while (min % 3 == 0 && min >= 3){
+            min /= 3;
+        }
+        if (min == 1) System.out.println("YES");
+        else System.out.println("NO");
     }
 }
